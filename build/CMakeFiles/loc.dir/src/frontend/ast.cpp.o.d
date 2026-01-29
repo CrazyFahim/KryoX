8eebd5a86560bafe608db7271ee2897330c9cd1d
Crazy Fahim <ahmedfhm015@gmail.com>
2026-01-29 22:22:01 +0600

    Ignore build folder

--- build/CMakeFiles/loc.dir/src/frontend/ast.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/loc.dir/src/frontend/ast.cpp.o: \
- /media/fahim/Dell\ External/All-About-Machine-Learning-and-Deep-Learing/NON-PAPER-implementation-Projects/ML-Compiler-Exploration/Linear-operator-compiler/src/frontend/ast.cpp \
- /usr/include/stdc-predef.h
--- build/all_test.sh
+++
@@ -1,5 +0,0 @@
-for f in ../examples/t*.loc; do
-  echo
-  echo "===== Running $f ====="
-  ./loc < "$f" || break
-done
